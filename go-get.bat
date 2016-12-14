379510529d4d5c68b3e905854025997ca977f732
toruuetani <toruuetani@gmail.com>
2016-12-14 22:58:28 +0900

    go get 用バッチ追加

---
+++ go-get.bat
@@ -0,0 +1,22 @@
+REM utility
+go get github.com/motemen/ghq
+go get github.com/peco/peco
+
+
+REM develop
+go get github.com/Masterminds/glide
+go get github.com/motemen/gore
+
+
+REM develop for visual studio code
+go get github.com/nsf/gocode
+go get github.com/rogpeppe/godef
+go get github.com/golang/lint/golint
+go get github.com/lukehoban/go-outline
+go get sourcegraph.com/sqs/goreturns
+go get golang.org/x/tools/cmd/gorename
+go get github.com/tpng/gopkgs
+go get github.com/newhook/go-symbols
+go get golang.org/x/tools/cmd/guru
+go get golang.org/x/tools/cmd/goimports
+go get github.com/derekparker/delve/cmd/dlv
